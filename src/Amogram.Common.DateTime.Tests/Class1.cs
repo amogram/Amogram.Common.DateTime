60a20baebcbfb0433b50d2b37c1cce7994e94ba1
Andrew McCaughan <andrew.mccaughan@gmail.com>
2016-09-08 09:53:17 +0100

    Initial Commit

---
+++ src/Amogram.Common.DateTime.Tests/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Amogram.Common.DateTime.Tests
+{
+    [TestFixture]
+    public class DateTimeHelperTests
+    {
+        [Test]
+        public void GetsCorrectSunday()
+        {
+            var dateTotest = new System.DateTime(2016, 09, 15);
+
+            var expectedNextSunday = new System.DateTime(2016, 09, 18);
+            var nextDate = dateTotest.GetNextOrToday(DayOfWeek.Sunday);
+
+            Assert.AreEqual(expectedNextSunday.Date, nextDate.Date);
+        }
+
+        [Test]
+        public void GetsSameDay()
+        {
+            var dateTotest = new System.DateTime(2016, 09, 18);
+
+            var expectedNextSunday = new System.DateTime(2016, 09, 18);
+            var nextDate = dateTotest.GetNextOrToday(DayOfWeek.Sunday);
+
+            Assert.AreEqual(expectedNextSunday.Date, nextDate.Date);
+        }
+    }
+}
---
+++ src/Amogram.Common.DateTime/DateTimeHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Amogram.Common.DateTime
+{
+    public static class DateTimeHelpers
+    {
+        /// <summary>
+        /// Gets the date for the next day.  If it's today, return today.
+        /// </summary>
+        /// <param name="from">Today's date</param>
+        /// <param name="dayOfWeek">Day of week</param>
+        /// <returns>Date of next day</returns>
+        public static System.DateTime GetNextOrToday(this System.DateTime from, DayOfWeek dayOfWeek)
+        {
+            if (from.DayOfWeek == dayOfWeek)
+            {
+                return from;
+            }
+
+            var start = (int)from.DayOfWeek;
+            var target = (int)dayOfWeek;
+
+            if (target <= start)
+            {
+                target += 7;
+            }
+
+            return from.AddDays(target - start);
+        }
+    }
+}
